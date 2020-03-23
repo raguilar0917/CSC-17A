ec41040b7fd8f8df6916e9e2d2f07f1f396c471e
Richard <codyrwa2011@me.com>
2020-03-22 20:00:53 -0700

    Finish Student List

--- Chapters 5 - 8 Review Assignment/StudentLineUp.cpp
+++ Chapters 5 - 8 Review Assignment/StudentLineUp.cpp
@@ -1,55 +1,67 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
+void printClass(string[], int);
+void getClass(string[], int);
+void swap(int, int);
+void bubbleSort(string[], int);
 
-void bubbleSort(string [], int);
-void Swap(char, char);
-void printList(string [], int size);
+int main() {
+    //this will hold the size of the class
+    int SIZE;
 
-int main(){
-    int classSize;
+    /*
+    the DO-WHILE will first ask how many student in the class if the size is
+    larger than 25 or less than 1 it will print an error and ask until paramters met
+    */
+    do {
+        cout << "Please enter the number of students: ";
+        cin >> SIZE;
+        if (SIZE > 25 || SIZE < 1) cout << "ERROR: INVALID ENTRY" << endl;
+    } while (SIZE > 25 || SIZE < 1);
 
-    do{
-        cout<<"Please enter class Size: ";
-        cin>>classSize;
-        if(classSize > 25 || classSize < 1) cout<<"ERROR: ENTER CLASS SIZE THAT IS BETWEEN 1-25"<<endl;
-    }while(classSize > 25 || classSize < 1);
+    //Create a dynamic array with the size inputted
+    string* classList = new string[SIZE];
+    getClass(classList, SIZE);
+    cout << "unsorted: ";
+    printClass(classList, SIZE);
+    bubbleSort(classList, SIZE);
+    cout << "sorted: ";
+    printClass(classList, SIZE);
 
-    string classList[classSize];
-    string sortList[classSize];
-
-    for(int i = 0; i < classSize; i++){
-        cout<<"Please enter student #"<<i+1<<": ";
-        cin>>classList[i];
-    }
-    bubbleSort(classList, classSize);
-    printList(classList, classSize);
+    delete[] classList;
+    return 0;
 }
-
-void printList(string arr[], int SIZE){
-    for(int i = 0; i < SIZE; i++){
-        cout<<arr[i][0]<<", ";
+void getClass(string arr[], int SIZE) {
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Please enter student #" << i + 1 << ": ";
+        cin >> arr[i];
     }
+}
+void printClass(string arr[], int SIZE) {
+    cout << "{";
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i];
+        if (i < SIZE - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "}" << endl;
 
 }
-
-
-void swap(char val1, char val2){
+void swap(int val1, int val2) {
     int temp = val1;
     val1 = val2;
     val2 = temp;
 }
-void bubbleSort(string arr[], int SIZE){
-    for(int i = 0; i < SIZE-1; i++){
-
-        for(int j = 0; j < SIZE ; j++){
-
-            if(arr[j][0] > arr[j+1][0]){
-                swap(arr[j], arr[j+1]);
-
+void bubbleSort(string arr[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
             }
-
         }
     }
 }
